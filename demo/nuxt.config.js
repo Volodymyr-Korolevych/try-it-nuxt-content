84322c4956c697be6914ffe5d465ab75c68dd2d2
Volodymyr <Volodymyr.Korolevych@gmail.com>
2021-08-09 02:04:33 +0300

    ssr fix (2)

--- demo/nuxt.config.js
+++ demo/nuxt.config.js
@@ -1,6 +1,6 @@
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  target: 'static',
+  // target: 'static',
   ssr: true,
 
   // Global page headers: https://go.nuxtjs.dev/config-head
